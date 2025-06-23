071c9e27f4a42cb126bc0bb861911e28c7538193
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-06-24 02:22:41 +0300

    Q1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            /*
+            1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            Console.WriteLine("please enter your number : ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 3 == 0 && x % 4 == 0)
+            {
+
+                Console.WriteLine(" YES ");
+            }
+            else { 
+            Console.WriteLine(" NO ");
+            }*/
+            #endregion
+
         }
     }
 }
